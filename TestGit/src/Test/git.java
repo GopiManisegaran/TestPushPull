b470c2a2f736b4b5a41bda7f8f3207055aff709b
GOPI <GOPI@DESKTOP-6EURG75>
2020-04-08 23:57:16 +0530

    modiied

--- TestGit/src/Test/git.java
+++ TestGit/src/Test/git.java
@@ -5,13 +5,6 @@ public class git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hi gopi");
-<<<<<<< HEAD
-System.out.println("hi gopi");
-=======
-		System.out.println("hi gopi");
-		System.out.println("hi gopi");
-		System.out.println("hi gopi");
->>>>>>> branch 'master' of https://github.com/GopiManisegaran/TestPushPull.git
-	}
 
+	}
 }
